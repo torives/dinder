9504567da47baab14bc25969b35af95a606544c5
Victor Yves Crispim <rotcivyves@gmail.com>
2020-01-08 23:08:05 -0300

    feat: move image with Dragable

--- lib/pages/MyHomePage.dart
+++ lib/pages/MyHomePage.dart
@@ -10,29 +10,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _visible = true;
+
   @override
   Widget build(BuildContext context) {
+    var image = Opacity(
+        opacity: _visible ? 1.0 : 0.0,
+        child: Image(
+          image: AssetImage("assets/yoda.jpg"),
+        ));
+    const image2 = Image(
+      image: AssetImage("assets/yoda.jpg"),
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: GestureDetector( 
-          onPanStart: (details) {
-              print("onPanStart");
-          },
-          onPanUpdate: (details) {
-            print("onPanUpdated ${details.globalPosition}");
-          },
-          onPanEnd: (details) {
-            print('onPanEnd');
-          },
-          child: Image(
-            image: AssetImage("assets/yoda.jpg"),
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
-      ),
-    ));
+        body: Center(
+            child: Draggable<Image>(
+              child: image,
+              feedback: image2,
+              onDragStarted: () {
+                print("onDragStarted");
+                setState(() => _visible = false);
+              },
+              onDragEnd: (_) {
+                print("onDragEnd");
+                setState(() => _visible = true);
+              },
+        )));
   }
 }
